38405b5ffdaa59508971d50de75b60fa28279714
KristianColville1 <kristiancolville1@gmail.com>
2025-04-21 17:32:51 +0100

    Test getting hourly wind speed for city

--- src/assets/js/utils/data_utils.js
+++ src/assets/js/utils/data_utils.js
@@ -15,3 +15,24 @@ export const getHourlyTemperature = (data, timestamp) => {
         ? `${data.hourly.apparent_temperature[index]}°C`
         : "N/A";
 };
+
+
+/**
+ * Get the current hourly wind speed
+ * @function getHourlyWindSpeed
+ * @param {Object} data - The weather data object
+ * @param {number} timestamp - The chosen time in the format iso8601
+ * @returns {string} - The current hourly wind speed
+ */
+export const getHourlyWindSpeed = (data, timestamp) => {
+
+    // Find the index of the current hour in the api data
+    const index = data.hourly.time.findIndex((time) =>
+        time.includes(timestamp)
+    );
+
+    // Using the index of the current hour, get the wind speed
+    return index !== -1
+        ? `${data.hourly.wind_speed_10m[index]} km/h`
+        : "N/A";
+};
