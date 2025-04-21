5aef2cec0c609c55d667706d32112fa7e3a27346
KristianColville1 <kristiancolville1@gmail.com>
2025-04-21 17:17:07 +0100

    Formulate utility method for retrieving hourly temperature

---
+++ src/assets/js/utils/data_utils.js
@@ -0,0 +1,11 @@
+/**
+ * Get the current hourly temperature
+ * @function getHourlyTemperature
+ * @param {Object} data - The weather data object
+ * @param {number} timestamp - The chosen time in the format iso8601
+ * @returns {string} - The current hourly temperature
+ */
+export const getHourlyTemperature = (data, timestamp) => {
+    // Find the index of the current hour in the api data
+    // Using the index of the current hour, get thetemperature
+};
