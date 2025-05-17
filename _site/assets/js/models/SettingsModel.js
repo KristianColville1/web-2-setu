480a4404985829c94d38ed1c888d45e5ae5ace22
KristianColville1 <kristiancolville1@gmail.com>
2025-05-17 11:14:04 +0100

    Tested local storage persistence and updating UI successful

--- _site/assets/js/models/SettingsModel.js
+++ _site/assets/js/models/SettingsModel.js
@@ -1,24 +1,47 @@
-
+import LocalStorageSerializer from "../utils/LocalStorageSerializer.js";
 
 export default class SettingsModel {
     constructor() {
-        this.favouriteCityList = JSON.parse(localStorage.getItem("favouriteCityList") || "[]");
+        this.serializer = new LocalStorageSerializer("favouriteCityList");
+        this.favouriteCityList = this.serializer.load() || [];
     }
 
     /**
-     * 
+     *
      * @returns {Array} - The list of favourite cities from local storage
      */
     getFavouriteCityList() {
         return this.favouriteCityList;
     }
 
-
     /**
      * Retrieves all the favourite city settings from local storage
      */
-    getFavouriteCitySettings(){
+    getFavouriteCitySettings() {
         return this.favouriteCityList;
     }
 
+    setFavouriteCityList(newList) {
+        this.favouriteCityList = newList;
+        this.serializer.save(newList);
+    }
+
+    addFavouriteCity(city) {
+        if (!this.favouriteCityList.includes(city)) {
+            this.favouriteCityList.push(city);
+            this.serializer.save(this.favouriteCityList);
+        }
+    }
+
+    removeFavouriteCity(city) {
+        this.favouriteCityList = this.favouriteCityList.filter(
+            (c) => c !== city
+        );
+        this.serializer.save(this.favouriteCityList);
+    }
+
+    clearFavouriteCities() {
+        this.favouriteCityList = [];
+        this.serializer.clear();
+    }
 }
--- src/assets/js/controllers/SettingsController.js
+++ src/assets/js/controllers/SettingsController.js
@@ -2,16 +2,24 @@ import SettingsModel from "../models/SettingsModel.js";
 import SettingsView from "../views/SettingsView.js";
 import CityFocusModel from "../models/CityFocusModel.js";
 export default class SettingsController {
-    init(){
+    init() {
         this.model = new SettingsModel();
         this.cities = CityFocusModel.retrieveCitiesListFormatted();
-        this.view = new SettingsView(this.cities, this.updateSettings.bind(this)); // bing the callback to the current context
-        this.view.renderFavouriteCitySettings(this.model.getFavouriteCitySettings());
+        this.view = new SettingsView(
+            this.cities,
+            this.updateFavouriteCitySettings.bind(this)
+        ); // bing the callback to the current context
+        this.view.renderFavouriteCitySettings(
+            this.model.getFavouriteCitySettings()
+        );
         console.log("SettingsController initialized");
     }
 
-    updateSettings(city, isChecked){
-        console.log("Callback worked for SettingsController");
-        console.log(`City: ${city}, Checked: ${isChecked}`);
+    updateFavouriteCitySettings(city, isChecked) {
+        if (isChecked) {
+            this.model.addFavouriteCity(city);
+        } else {
+            this.model.removeFavouriteCity(city);
+        }
     }
 }
